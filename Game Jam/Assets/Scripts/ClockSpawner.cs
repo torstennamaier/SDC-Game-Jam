2e7c55105b218a1458fbabd1b33bb65c14a8d823
Daniel-Nimitz <63825442+Daniel-Nimitz@users.noreply.github.com>
2020-05-01 20:59:37 -0600

    Clock and Enemy Spawners
    There is an enemy spawner which spawns enemies, it is in prefabs so we can mae multiple portals if we want.
    
    There is a clock spawner which spawns primitive clocks which give ammo instead of time right now (will need to be changed when we get clocks)

---
+++ Game Jam/Assets/Scripts/ClockSpawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClockSpawner : MonoBehaviour
+{
+    //This prefab clock is set manually from the unity editor
+    public GameObject clock;
+    //This is how long it takes before we start clocks
+    public float spawnDelay = 2.0f;
+    //This is how many seconds it is between each time we spawn a clock
+    public float spawnInterval = 2.0f;
+    //These help us decide the position of the clock to spawn
+    public float spawnRangeX = 130.0f;
+    public float spawnRangeZ = 60.0f;
+    public float spawnPosY = 25.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //When the game starts then we starts spawning enemies from the array assigned every spawnInterval number of seconds
+        InvokeRepeating("SpawnClock", spawnDelay, spawnInterval);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    //We will spawn a clock at a random x and z position, the y will always be the same
+    void SpawnClock()
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), spawnPosY, Random.Range(-spawnRangeZ, spawnRangeZ));
+        Instantiate(clock, spawnPos, clock.transform.rotation);
+    }
+}
+
+/*Note from Daniel:  To make this script work we must maually assign a prefab to spawn from the unity editor.
+ * The assigned prefab will be what the spawner creates.
+ * We can use this same spawner scripts to spawn benificial items as well.  They would only spawn in a certain position though.*/
+
---
+++ Game Jam/Assets/Scripts/EnemySpawnManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    //This is an array we manually can add different enemy prefabs to, in case we have multiple enemy types
+    public GameObject[] enemyPrefabs;
+    //This is how long it takes before we start spawning enemies in seconds
+    public float spawnDelay = 2.0f;
+    //This is how many seconds it is between each time we spawn an enemy
+    public float spawnInterval = 2.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //When the game starts then we starts spawning enemies from the array assigned every spawnInterval number of seconds
+        InvokeRepeating("SpawnRandomEnemy", spawnDelay, spawnInterval);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //We will spawn a randomly selected enemy from the position of the object the script is attached to when this is called
+    void SpawnRandomEnemy()
+    {
+        int enemyIndex = Random.Range(0, enemyPrefabs.Length);
+        Vector3 spawnPos = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+        Instantiate(enemyPrefabs[enemyIndex], spawnPos, enemyPrefabs[enemyIndex].transform.rotation);
+
+    }
+}
+
+/*Note from Daniel:  To make this script work we must maually assign a prefab to spawn from the unity editor.
+ * The assigned prefab will be what the spawner creates.
+ * We can use this same spawner scripts to spawn benificial items as well.  They would only spawn in a certain position though.*/
