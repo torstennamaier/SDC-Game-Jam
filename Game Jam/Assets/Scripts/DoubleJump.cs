d01ab8f92f48d0787d44031b17c7f966d6a39b84
torstennamaier <torstennamaier@outlook.com>
2020-05-02 10:36:06 -0400

    Double Jump Added

---
+++ Game Jam/Assets/Scripts/DoubleJump.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class DoubleJump : MonoBehaviour
+{
+    [SerializeField] float doubleJumpForce = 30f;
+
+    RigidbodyFirstPersonController target;
+    bool doubleJump = false;
+    Rigidbody m_RigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = FindObjectOfType<PlayerHealth>().GetComponent<RigidbodyFirstPersonController>();
+        m_RigidBody = target.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            if(target.Jumping && doubleJump == false)
+            {
+                m_RigidBody.drag = 0f;
+                m_RigidBody.velocity = new Vector3(m_RigidBody.velocity.x, 0f, m_RigidBody.velocity.z);
+                m_RigidBody.AddForce(new Vector3(0f, doubleJumpForce, 0f), ForceMode.Impulse);
+                doubleJump = true;
+            }
+            else
+            {
+                return;
+            }
+        }
+        if (target.Grounded)
+        {
+            doubleJump = false;
+        }
+    }
+}
